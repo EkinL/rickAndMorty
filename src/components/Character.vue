5ee46d631605927e8548509adc898a28a0c44ac0
Lilian Hammache <fayzerdev@gmail.com>
2025-04-02 21:26:06 +0200

    initialize project structure with Vue, Vite, and Tailwind CSS; add basic components and routing

---
+++ src/components/Character.vue
@@ -0,0 +1,79 @@
+<script setup>
+import { ref } from 'vue'
+import axios from 'axios'
+
+const characters = ref([])
+const currentPage = ref(1)
+const totalPages = ref(0)
+
+const fetchCharacters = (page = 1) => {
+  axios
+    .get(`https://rickandmortyapi.com/api/character?page=${page}`)
+    .then((response) => {
+      characters.value = response.data.results
+      totalPages.value = response.data.info.pages // Stockez le nombre total de pages
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+}
+
+fetchCharacters(currentPage.value)
+
+// Fonction pour aller à la page suivante
+const nextPage = () => {
+  if (currentPage.value < totalPages.value) {
+    currentPage.value++
+    fetchCharacters(currentPage.value)
+  }
+}
+
+const previousPage = () => {
+  if (currentPage.value > 1) {
+    currentPage.value--
+    fetchCharacters(currentPage.value)
+  }
+}
+</script>
+
+<template>
+  <div class="p-12 bg-gray-100 min-h-screen">
+    <h1 class="text-3xl font-bold text-center mb-6">Character Component</h1>
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+      <div
+        v-for="character in characters"
+        :key="character.id"
+        class="bg-white shadow-md rounded-lg overflow-hidden transform transition hover:scale-105 hover:shadow-lg cursor-pointer"
+      >
+        <div class="relative w-full h-64">
+          <img
+            :src="character.image"
+            alt="Character Image"
+            class="absolute inset-0 w-full h-full object-cover"
+          />
+        </div>
+        <div class="p-4">
+          <h2 class="text-xl font-semibold text-gray-800">{{ character.name }}</h2>
+          <p class="text-gray-600">Status: {{ character.status }}</p>
+          <p class="text-gray-600">Species: {{ character.species }}</p>
+        </div>
+      </div>
+    </div>
+    <div class="flex justify-center mt-6 space-x-4">
+      <button
+        @click="previousPage"
+        :disabled="currentPage <= 1"
+        class="px-4 py-2 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600 disabled:opacity-50 disabled:cursor-not-allowed"
+      >
+        Previous Page
+      </button>
+      <button
+        @click="nextPage"
+        :disabled="currentPage >= totalPages"
+        class="px-4 py-2 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600 disabled:opacity-50 disabled:cursor-not-allowed"
+      >
+        Next Page
+      </button>
+    </div>
+  </div>
+</template>
---
+++ src/views/CharacterView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Character from '@/components/Character.vue'
+</script>
+
+<template>
+  <div class="">
+    <h1>This is an Character page</h1>
+
+    <Character />
+  </div>
+</template>

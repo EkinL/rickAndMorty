f855a0fd7e92822eb062572d3e97408cf0273964
Lilian Hammache <fayzerdev@gmail.com>
2025-04-02 23:12:20 +0200

    🚧  - enhance SearchBar component styles for better visibility in dark mode

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -59,7 +59,7 @@ const goToCharacterDetail = (id) => {
         v-for="character in searchResults"
         :key="character.id"
         @click="goToCharacterDetail(character.id)"
-        class="cursor-pointer px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600"
+        class="cursor-pointer px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-white text-gray-900 dark:text-gray-300"
       >
         {{ character.name }}
       </li>
